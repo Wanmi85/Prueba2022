8a6cfc7bb4081fc24e5dcbc13f6295cd88ef9365
Wanmi85 <wannmi2405@gmail.com>
2022-10-05 12:04:08 +0200

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'joke-list',
+  
+
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class JokeComponent {
   title = 'Prueba2022';
+  
+  jokes: Object[];
+  constructor() {
+  this.jokes = [
+  {
+  setup: "What did the cheese say when it looked in the mirror?",
+  punchline: "Hello-Me (Halloumi)"
+  },
+  {
+  setup: "What kind of cheese do you use to disguise a small horse?",
+  punchline: "Mask-a-pony (Mascarpone)"
+  },
+  {
+  setup: "A kid threw a lump of cheddar at me",
+  punchline: "I thought ‘That’s not very mature’"
+  },
+  ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { JokeComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    JokeComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [JokeComponent]
 })
 export class AppModule { }
